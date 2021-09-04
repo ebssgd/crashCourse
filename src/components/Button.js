6d929cd2b934f13766ab39d4d648cceb4ed97a25
ebssgd <iamebssgd@yahoo.com>
2021-09-04 11:03:51 -0500

    Final commit, everything fully functional

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,15 +2,13 @@ import PropTypes from "prop-types";
 
 const Button = ({ color, text, onClick }) => {
   return (
-    <div>
-      <button
-        onClick={onClick}
-        style={{ backgroundColor: color }}
-        className="btn"
-      >
-        {text}
-      </button>
-    </div>
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className="btn"
+    >
+      {text}
+    </button>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
 import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
 import Button from "./Button";
 
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log("Click");
-  };
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
+
   return (
-    <div>
-      <header className="header">
-        <h1>{title}</h1>
-        <Button color="green" text="Add" onClick={onClick} />
-      </header>
-    </div>
+    <header className="header">
+      <h1>{title}</h1>
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Close" : "Add"}
+          onClick={onAdd}
+        />
+      )}
+    </header>
   );
 };
 
@@ -22,5 +26,11 @@ Header.defaultProps = {
 Header.propTypes = {
   title: PropTypes.string.isRequired,
 };
+
+// CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black',
+// }
 
 export default Header;
